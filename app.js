f734cd329c71916a3b9f070181e26bb129c8ee9b
M Shubar <70318400+MShubar@users.noreply.github.com>
2024-10-03 14:51:19 +0300

    Update

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 // 1) I want to make the user enter the username and then start the game by clicking on a button which stores the username and uses it during the game
 
 // a) Create an event listener for "startGameBtn" that listens for clicks and runs a function called "storeUsername".
-// b) Inside the "storeUsername" function:
-// Take the value from the "usernameInput" field and store it in a variable called "username".
-// If no username is entered, show an alert asking the user to input a name.
-// Display the username in the "messageDisplay" area and call the "startGame" function to begin the game.
+// b) Inside the "storeUsername" function. Take the value from the "usernameInput" field and store it in a variable called "username". If no username is entered, show an alert asking the user to input a name. Display the username in the "messageDisplay" area and call the "startGame" function to begin the game.
 
 // 2) Make a keyboard that contains letters only and stores the guessed letters
 
@@ -23,17 +20,14 @@
 // 4) Start the game and choose a random word
 
 // a) Inside the "startGame" function, select a random word from the "words" array and store it in the variable "currentWord".
-// b) Initialize the "correctGuesses" array with underscores, where each underscore represents an unguessed letter in the "currentWord".
+// b) the "correctGuesses" array should have underscores, where each underscore represents an unguessed letter in the "currentWord".
 // c) Reset the "wrongGuesses" array to clear any previous wrong guesses.
 // d) Call the "renderWord" function to display the underscores on the screen representing the unguessed letters.
 
 // 5) Rendering the word and handling guesses
 
 // a) The "renderWord" function should update the display by showing the correctly guessed letters and underscores for letters that haven't been guessed.
-// b) In the "handleGuess" function:
-// Check if the guessed letter exists in "currentWord".
-// If it does, update the "correctGuesses" array to replace the appropriate underscores with the guessed letter.
-// If it doesn’t, add the guessed letter to "wrongGuesses" and check if the player has reached the loss condition (too many wrong guesses).
+// b) In the "handleGuess" function. Check if the guessed letter exists in "currentWord". If it does, update the "correctGuesses" array to replace the appropriate underscores with the guessed letter. If it doesn’t, add the guessed letter to "wrongGuesses" and check if the player has reached the loss condition (too many wrong guesses).
 // c) After each guess, call either "checkWin" or "checkLoss" to determine if the game has been won or lost.
 
 // 6) Checking if the player won or lost
