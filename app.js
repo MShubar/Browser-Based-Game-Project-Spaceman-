316b6f5045af62c9a071fc9c377ea86c531c7d25
MShubar <pokeeeeepow@gmail.com>
2024-10-07 20:54:02 +0300

    (07-10-2024-01)(I have done some of the javascipt)(I will continue later)

--- app.js
+++ app.js
@@ -1,49 +1,69 @@
-// 1) I want to make the user enter the username and then start the game by clicking on a button which stores the username and uses it during the game
+// -------------------------------------Variables---------------------------------------//
+let username
+let clickedLetters = []
+let winCount = 0
+let lossCount = 0
+let currentWord
+// -------------------------------------Constants----------------------------------------//
+const words = ['hi', 'hey']
 
-// a) Create an event listener for "startGameBtn" that listens for clicks and runs a function called "storeUsername".
+// ---------------------------------Chashed elements-------------------------------------//
+// 1
+const startGameBtn = document.querySelector('#startgame')
+const usernameInput = document.querySelector('#inputusername')
+const messageDisplay = document.querySelector('#messagedisplay')
+//
+// 2
+const keyboard = document.querySelectorAll('.keyboard')
+//
+// 3
+const winDisplay = document.querySelector('#wincount')
+const lossDisplay = document.querySelector('#losscount')
+// ---------------------------------Functions-------------------------------------------//
+// 1
+const storeUsername = (event) => {
+  console.log(event.target.id)
+  username = usernameInput.textContent
+  if (username !== '') {
+    user
+  } else {
+  }
+}
+//
+// 2
+const handleGuess = (event) => {
+  clickedLetter = event.target.id
+  clickedLetters.push(clickedLetter)
+  event.target.setAttribute('disabled', '')
+  console.log(clickedLetters)
+  scoreDisplay()
+}
+//
+// 3
+const scoreDisplay = () => {
+  winDisplay.textContent = winCount
+  lossDisplay.textContent = lossCount
+}
+//
+// 5
+const renderWord = clickedLetters.some((clickedLetter, index) => {})
+//
+// 6
+const winCheck = () => {}
+//
+//
+const randomWord = () => {
+  currentWord = words[parseInt(Math.random() * 3)]
+  console.log(currentWord)
+}
+// -------------------------------Event Listerners-------------------------------------//
+randomWord()
 
-// b) Inside the "storeUsername" function:
-// Take the value from the "usernameInput" field and store it in a variable called "username".
-// If no username is entered, show an alert asking the user to input a name.
-// Display the username in the "messageDisplay" area and call the "startGame" function to begin the game.
-
-// b) Inside the "storeUsername" function. Take the value from the "usernameInput" field and store it in a variable called "username". If no username is entered, show an alert asking the user to input a name. Display the username in the "messageDisplay" area and call the "startGame" function to begin the game.
-
-// 2) Make a keyboard that contains letters only and stores the guessed letters
-
-// a) Dynamically create buttons for each letter of the alphabet and append them to the "keyboard" container.
-// b) Add an event listener to each letter button that listens for clicks.
-// c) When clicked, the button should call the "handleGuess" function with the clicked letter passed as an argument.
-// d) Once a letter is clicked, disable the button to prevent it from being clicked again.
-
-// 3) Showing the username and the score next to it
-
-// a) Display the player's username in the "messageDisplay" section when the game starts.
-// b) Create two variables, "winCount" and "lossCount", to store and keep track of the player's wins and losses.
-// c) Show the current win and loss counts in the "winDisplay" and "lossDisplay" sections.
-// d) Update these counts whenever the player wins or loses a game, reflecting the updated scores.
-
-// 4) Start the game and choose a random word
-
-// a) Inside the "startGame" function, select a random word from the "words" array and store it in the variable "currentWord".
-// b) the "correctGuesses" array should have underscores, where each underscore represents an unguessed letter in the "currentWord".
-// c) Reset the "wrongGuesses" array to clear any previous wrong guesses.
-// d) Call the "renderWord" function to display the underscores on the screen representing the unguessed letters.
-
-// 5) Rendering the word and handling guesses
-
-// a) The "renderWord" function should update the display by showing the correctly guessed letters and underscores for letters that haven't been guessed.
-// b) In the "handleGuess" function. Check if the guessed letter exists in "currentWord". If it does, update the "correctGuesses" array to replace the appropriate underscores with the guessed letter. If it doesn’t, add the guessed letter to "wrongGuesses" and check if the player has reached the loss condition (too many wrong guesses).
-// c) After each guess, call either "checkWin" or "checkLoss" to determine if the game has been won or lost.
-
-// 6) Checking if the player won or lost
-
-// a) The "checkWin" function checks if the "correctGuesses" array no longer contains any underscores. If it doesn’t, the player has won.
-// b) If the player wins, update the win count, display a win message, and make the "restartBtn" visible.
-// c) The "checkLoss" function checks if the length of "wrongGuesses" is 6 or more, meaning the player has lost.
-// d) If the player loses, update the loss count, display the correct word, and show a loss message. The "restartBtn" should also become visible.
-
-// 7) Restarting the game
-
-// a) Make the "restartBtn" visible when the player wins or loses the game.
-// b) Add an event listener to the "restartBtn" that will reset the game by calling the "startGame" function again.
+// 1
+startGameBtn.addEventListener('click', storeUsername)
+//
+// 2
+keyboard.forEach((letter) => {
+  letter.addEventListener('click', handleGuess)
+})
+//
